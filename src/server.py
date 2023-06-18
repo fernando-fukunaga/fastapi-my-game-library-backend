76aff1bd7ee507d09a05f1754af1745a976ac22a
Fernando Fukunaga <fernandosantanna.2457@saojudas.aluno.br>
2023-06-18 08:10:18 -0300

    primeiros post e get da aplicação funcionando

--- src/server.py
+++ src/server.py
@@ -7,11 +7,12 @@ from src.infra.sqlalchemy.repositorios.usuario import RepositorioUsuario
 criar_banco_de_dados()
 app = FastAPI()
 
-@app.get("/")
-def helloworld():
-    return "hello world"
-
-@app.post("/cadastro")
+@app.post("/cadastrar-usuarios")
 def cadastar(usuario: Usuario, banco_de_dados: Session = Depends(get_banco_de_dados)):
     usuario_cadastrado = RepositorioUsuario(banco_de_dados).criar(usuario)
     return usuario_cadastrado
+
+@app.get("/listar-usuarios")
+def listar(banco_de_dados: Session = Depends(get_banco_de_dados)):
+    lista_usuarios = RepositorioUsuario(banco_de_dados).listar()
+    return lista_usuarios
