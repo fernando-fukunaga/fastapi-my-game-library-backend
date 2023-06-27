1d7d0a086fac0336e57f2fc1e7aa796bcd212be8
Fernando Fukunaga <fanna@brasilseg.com.br>
2023-06-27 17:24:56 -0300

    introduzindo o alembic no projeto e também fiz algumas alterações nas regras de negocio do banco de dados

--- src/infra/sqlalchemy/models/models.py
+++ src/infra/sqlalchemy/models/models.py
@@ -1,30 +1,30 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, DECIMAL, ForeignKey
 from src.infra.sqlalchemy.config.database import Base
 
 class Usuario(Base):
     __tablename__ = "usuario"
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String)
-    email = Column(String)
-    username = Column(String)
-    senha = Column(String)
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(55))
+    email = Column(String(45), unique=True)
+    username = Column(String(14), unique=True)
+    senha = Column(String(100))
 
 class Plataforma(Base):
     __tablename__ = "plataforma"
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String)
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(55))
     id_usuario = Column(Integer, ForeignKey("usuario.id"))
-    fabricante = Column(String)
-    observacoes = Column(String)
+    fabricante = Column(String(15))
+    observacoes = Column(String(100))
 
 class Jogo(Base):
     __tablename__ = "jogo"
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String)
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(55))
     id_plataforma = Column(Integer, ForeignKey("plataforma.id"))
     ano = Column(Integer)
-    categoria = Column(String)
-    desenvolvedora = Column(String)
+    categoria = Column(String(20))
+    desenvolvedora = Column(String(15))
     id_usuario = Column(Integer, ForeignKey("usuario.id"))
-    observacoes = Column(String)
-    status = Column(String)
+    observacoes = Column(String(100))
+    progresso = Column(DECIMAL(3,2))
