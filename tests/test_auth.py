7b8a35a0692e74d70a6bdb02e150d9a1f9864e2a
fernando-fukunaga <fernandofukunaga14@gmail.com>
2023-12-19 16:51:12 -0300

    Improving unit tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,11 @@
 # Testes para endpoints de autenticação
 from fastapi.testclient import TestClient
 from src.main import app
-from tests.gerador_numeros import gera_numero
 from unittest.mock import patch, MagicMock
+from passlib.context import CryptContext
+
+# Criando contexto de bcrypt para senhas de usuarios mockados
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 """Criando um testclient para fazer requisições sem precisar subir o servidor,
 isso aumenta muito o desempenho dos testes, ele usa o HTTPX
@@ -14,7 +17,7 @@ TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZlcm5hbmRvIiwidm
 headers = {"Authorization": f"Bearer {TOKEN}"}
 
 
-class TestSignUp():
+class TestSignUp:
 
     @patch("src.services.services_auth.RepositorioUsuario.insert_usuario")
     @patch("src.services.services_auth.RepositorioUsuario.select_usuario")
@@ -35,56 +38,73 @@ class TestSignUp():
         mock_select.assert_called()
         mock_insert.assert_called_once()
         assert response.status_code == 201
-        assert response.json()['nome'] == 'daniel'
 
-    def test_criar_usuario_com_email_existente_retorna_400(self):
+    @patch("src.services.services_auth.RepositorioUsuario.select_usuario")    
+    def test_criar_usuario_com_email_ou_username_existentes_retorna_400(self, mock_select):
+        mock_user_model = MagicMock(nome='daniel',
+                                    email='daniel',
+                                    username='daniel',
+                                    senha='daniel')
+        mock_select.return_value = mock_user_model
+
         response = client.post(url="/auth/signup",
-                               json={"nome": "Test Dummy",
-                                     "email": "fernando@email.com",
-                                     "username": f"test{gera_numero()}",
-                                     "senha": "senha"})
+                               json={"nome": "daniel",
+                                     "email": "daniel",
+                                     "username": "daniel",
+                                     "senha": "daniel"})
 
-        assert response.status_code == 400
-
-    def test_criar_usuario_com_username_existente_retorna_400(self):
-        response = client.post(url="/auth/signup",
-                               json={"nome": "Test Dummy",
-                                     "email": f"test{gera_numero()}@email.com",
-                                     "username": "fernando",
-                                     "senha": "senha"})
-
+        mock_select.assert_called()
         assert response.status_code == 400
 
     def test_criar_usuario_com_username_maior_que_14_chars_retorna_400(self):
         response = client.post(url="/auth/signup",
-                               json={"nome": "Test Dummy",
-                                     "email": f"test{gera_numero()}@email.com",
-                                     "username": "test11111111111",
-                                     "senha": "senha"})
+                               json={"nome": "daniel",
+                                     "email": "daniel",
+                                     "username": "daniel000000000",
+                                     "senha": "daniel"})
 
         assert response.status_code == 400
 
 
 class TestLogin:
 
-    def test_tentar_login_com_usuario_existente_retorna_200(self):
+    @patch("src.services.services_auth.RepositorioUsuario.select_usuario")
+    def test_tentar_login_com_usuario_existente_retorna_200(self, mock_select):
+        mock_user_model = MagicMock(nome='daniel',
+                                    email='daniel',
+                                    username='daniel',
+                                    senha=password_context.hash('daniel'))
+        mock_select.return_value = mock_user_model
+        
         response = client.post(url="/auth/login",
-                               data={"username": "fernando",
-                                     "password": "senha"})
+                               data={"username": "daniel",
+                                     "password": "daniel"})
 
-        assert response.status_code == 200
+        mock_select.assert_called()
+        assert response.status_code == 200 
 
-    def test_tentar_login_com_usuario_inexistente_retorna_400(self):
+    @patch("src.services.services_auth.RepositorioUsuario.select_usuario")
+    def test_tentar_login_com_usuario_inexistente_retorna_400(self, mock_select):
+        mock_select.return_value = None
+        
         response = client.post(url="/auth/login",
-                               data={"username": "usuario_inexistente",
-                                     "password": "senha"})
+                               data={"username": "daniel",
+                                     "password": "daniel"})
 
+        mock_select.assert_called()
         assert response.status_code == 400
 
-    def test_tentar_login_com_senha_incorreta_retorna_400(self):
+    @patch("src.services.services_auth.RepositorioUsuario.select_usuario")
+    def test_tentar_login_com_senha_incorreta_retorna_400(self, mock_select):
+        mock_user_model = MagicMock(nome='daniel',
+                                    email='daniel',
+                                    username='daniel',
+                                    senha=password_context.hash('daniel'))
+        mock_select.return_value = mock_user_model
+
         response = client.post(url="/auth/login",
-                               data={"username": "fernando",
-                                     "password": "senha1"})
+                               data={"username": "daniel",
+                                     "password": "daniel0"})
 
         assert response.status_code == 400
 
