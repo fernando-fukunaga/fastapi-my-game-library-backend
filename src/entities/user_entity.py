9e12cc0ada0bb40f65706eb26268b35ce9fd0d33
fernando-fukunaga <fernandofukunaga14@gmail.com>
2024-09-17 17:08:44 -0300

    Updating User model

--- src/entities/user_entity.py
+++ src/entities/user_entity.py
@@ -1,6 +1,13 @@
 class UserEntity:
-    id: int
+    id: str
     name: str
     email: str
     username: str
     password: str
+
+    def __init__(self, id, name, email, username, password):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
--- src/infra/database/models/sqlalchemy_models.py
+++ src/infra/database/models/sqlalchemy_models.py
@@ -4,31 +4,18 @@ from src.infra.database.config.database import Base
 
 
 class UserSQLAlchemyModel(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(36), unique=True)
     name = Column(String(55))
     email = Column(String(45), unique=True)
     username = Column(String(14), unique=True)
     password = Column(String(100))
 
-    platforms = relationship("Plataforma", back_populates="user")
+    platforms = relationship("PlatformSQLALchemyModel", back_populates="users")
 
 
 class Plataforma(Base):
-    """Representa a plataforma de algum usuário.
-
-    Será usada pelos repositórios que interagem com o banco.
-
-    Attributes:
-        id (int): número de identificação plataforma
-        nome (str): nome da plataforma
-        id_usuario (int): id do usuário a qual a plataforma pertence
-        fabricante (str): nome da empresa que fabrica essa plataforma
-        observacoes (str): obervações adicionais
-        usuario (UserSQLAlchemyModel): a pessoa dona da plataforma
-        jogos (List[Jogo]): lista de jogos desta plataforma
-    """
     __tablename__ = "plataforma"
 
     id = Column(Integer, primary_key=True)
@@ -42,21 +29,6 @@ class Plataforma(Base):
 
 
 class Jogo(Base):
-    """Representa os jogos de algum usuário.
-
-    Será usada pelos repositórios que interagem com o banco.
-
-    Attributes:
-        id (int): número de identificação jogo
-        nome (str): nome djogo
-        id_plataforma (int): id da plataforma a qual o jogo pertence
-        ano (int): ano de lançamento do jogo
-        categoria (str): categoria do jogo
-        desenvolvedora (str): nome da desenvolvedora
-        observacoes (str): obervações adicionais
-        progresso (float): progresso do usuário nesse jogo
-        plataforma (Plataforma): plataforma a qual o jogo está atrelado
-    """
     __tablename__ = "jogo"
 
     id = Column(Integer, primary_key=True)
